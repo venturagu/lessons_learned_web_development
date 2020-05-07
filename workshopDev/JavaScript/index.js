c2d3b0b7100831140e3cbd78908207b0281b08e7
Gustavo Ventura <gustavo.ventura100@gmail.com>
2020-05-07 15:55:03 -0300

    Inicialização do projeto

---
+++ workshopDev/JavaScript/index.js
@@ -0,0 +1,11 @@
+function onOff(){
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+}
+
+
+document
+    .querySelector("button.fat")
+    .addEventListener("click", onOff )
